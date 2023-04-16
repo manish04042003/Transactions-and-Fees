e01f8ecee668118e375879010b0c1f8f1e25faaf
manish04042003 <manishkumar817835@gmail.com>
2023-04-17 02:04:50 +0530

    error occure but check balance function is working

---
+++ chalange.js
@@ -0,0 +1,106 @@
+// Import Solana web3 functinalities
+const {
+  Connection,
+  PublicKey,
+  clusterApiUrl,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  Transaction,
+  SystemProgram,
+  sendAndConfirmRawTransaction,
+  sendAndConfirmTransaction,
+} = require("@solana/web3.js");
+
+// paste your secret that is logged here
+const DEMO_FROM_SECRET_KEY = new Uint8Array(
+  // paste your secret key array here
+  [
+    155, 239, 101, 41, 106, 222, 103, 103, 224, 69, 37, 204, 94, 255, 172, 92,
+    136, 30, 63, 15, 186, 13, 16, 174, 172, 212, 41, 227, 41, 16, 83, 27, 104,
+    121, 168, 159, 183, 100, 60, 23, 161, 119, 98, 48, 19, 159, 131, 213, 18,
+    71, 30, 96, 156, 13, 65, 7, 25, 237, 18, 249, 226, 37, 142, 27,
+  ]
+);
+
+// Get Keypair from Secret Key
+var from = Keypair.fromSecretKey(DEMO_FROM_SECRET_KEY);
+
+let transfermoney = 0;
+const getWalletBalance = async () => {
+  try {
+    // Connect to the Devnet
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    // console.log("Connection object is:", connection);
+
+    // Make a wallet (keypair) from privateKey and get its balance
+    // const myWallet = await Keypair.fromSecretKey(DEMO_FROM_SECRET_KEY);
+    // console.log(from);
+    const walletBalance = await connection.getBalance(
+      new PublicKey(from.publicKey)
+    );
+    console.log(
+      `Wallet balance: ${parseInt(walletBalance) / LAMPORTS_PER_SOL} SOL`
+    );
+    transfermoney = parseInt(walletBalance) / LAMPORTS_PER_SOL;
+    // console.log(transfermoney);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// getWalletBalance();
+
+const transferSol = async () => {
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+  // Other things to try:
+  // 1) Form array from userSecretKey
+  // const from = Keypair.fromSecretKey(Uint8Array.from(userSecretKey));
+  // 2) Make a new Keypair (starts with 0 SOL)
+  // const from = Keypair.generate();
+
+  // Generate another Keypair (account we'll be sending to)
+  const to = Keypair.generate();
+
+  // transfer 50% of the balance to another wallet.
+  console.log("Airdopping some SOL to Sender wallet!");
+//   await getWalletBalance();
+//   transfermoney = transfermoney / 2;
+  console.log('Half tranfer money ',transfermoney);
+  const fromAirDropSignature = await connection.requestAirdrop(
+    new PublicKey(from.publicKey),
+    0.1 * LAMPORTS_PER_SOL
+  );
+
+  // Latest blockhash (unique identifer of the block) of the cluster
+  let latestBlockHash = await connection.getLatestBlockhash();
+
+  // Confirm transaction using the last valid block height (refers to its time)
+  // to check for transaction expiration
+  await connection.confirmTransaction({
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    signature: fromAirDropSignature,
+  });
+
+  console.log("Airdrop completed for the Sender account");
+
+  // Send money from "from" wallet and into "to" wallet
+  var transaction = new Transaction().add(
+    SystemProgram.transfer({
+      fromPubkey: from.publicKey,
+      toPubkey: to.publicKey,
+      lamports: LAMPORTS_PER_SOL / 100,
+    })
+  );
+
+  // Sign transaction
+  var signature = await sendAndConfirmTransaction(connection, transaction, [
+    from,
+  ]);
+  console.log("Signature is", signature);
+
+  getWalletBalance();
+};
+
+transferSol();
